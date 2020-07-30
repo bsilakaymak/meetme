cd337002c6b04a61709538393f871d283ace54c8
Ali Amouri Kadhim <alikadhim87nl@gmail.com>
2020-07-30 15:24:00 +0200

    add user model

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+  },
+  companyName: {
+    type: String,
+    trim: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+  },
+  meetings: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: "Meeting",
+  },
+});
+module.exports = User = mongoose.model("User", userSchema);
