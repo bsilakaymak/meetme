092227005fb8aba3f77c64ef8a8d701dbd70e527
bsilakaymak <basaksilakaymak@gmail.com>
2020-07-30 14:43:25 +0200

    frontend component structure added

---
+++ frontend/src/components/Meetings/CreateMeeting.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CreateMeeting = ()=>{
+
+}
+
+export default CreateMeeting
---
+++ frontend/src/components/User/MeetingOverview.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MeetingOverview = ()=>{
+
+}
+
+export default MeetingOverview
