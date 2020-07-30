d41ee2326de9b589120f2ceb646fd97ad4924951
Rabee <rabeerd@gmail.com>
2020-07-30 15:03:08 +0200

    server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+
+connectDB();
+// Parse incoming request bodies
+app.use(express.json({ extended: false }));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
