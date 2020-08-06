26686117d5bec614a848a5f2d3a6f0d270f8e345
bsilakaymak <basaksilakaymak@gmail.com>
2020-08-06 19:56:28 +0200

    errors in routes solved

--- frontend/src/components/Routes/Routes.tsx
+++ frontend/src/components/Routes/Routes.tsx
@@ -1,12 +1,12 @@
-import React, { useContext } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Landing from '../LandingPage/Landing';
-import MeetingOverview from '../User/MeetingOverview';
-import CreateMeeting from '../Meetings/CreateMeeting';
-import MeetingDetails from '../Meetings/MeetingDetails';
-import Navigation from '../Shared/Navigation';
-import authContext from '../../context/authContext/authContext';
-import AuthState from '../../context/authContext/AuthState';
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Landing from "../LandingPage/Landing";
+import MeetingOverview from "../User/MeetingOverview";
+import CreateMeeting from "../Meetings/CreateMeeting";
+import MeetingDetails from "../Meetings/MeetingDetails";
+import Navigation from "../Shared/Navigation";
+import authContext from "../../context/authContext/authContext";
+import AuthState from "../../context/authContext/AuthState";
 
 interface Props {}
 
@@ -14,9 +14,10 @@ const Routes = (props: Props) => {
   const auth = useContext(authContext);
   return (
     <AuthState>
-      <Navigation />
       <Router>
-        {auth['isAuthenticated'] && <Navigation />}
+        {" "}
+        <Navigation />
+        {auth && auth["isAuthenticated"] && <Navigation />}
         <Switch>
           <Route path="/" component={Landing} exact />
           <Route path="/meeting-overview" component={MeetingOverview} exact />
